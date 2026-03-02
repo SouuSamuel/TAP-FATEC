8ee09c4d977dfee4529ca84034d1e1d2f2de2167
Samuel Jesus <samueljgn08@gmail.com>
2026-03-02 14:30:15 -0300

    Add Files (TAP0

---
+++ helloworld/src/main/java/com/fatec/aula01/helloworld/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.fatec.aula01.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+   public String hello() {
+    return "Hello, world!";
+   }
+
+}
